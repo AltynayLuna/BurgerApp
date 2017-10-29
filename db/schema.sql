9c87019366da4d51710a02fe84fe81eae4439d83
Luna <altynaylinstrum@yahoo.com>
2017-10-28 21:51:44 -0700

    Another round of updates

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE burgers
 	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255),
     devoured BOOLEAN DEFAULT false,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
 SELECT * FROM burgers
